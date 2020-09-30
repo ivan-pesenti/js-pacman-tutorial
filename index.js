c364599cc0a0aa22ff6838bb8b5d027c689c4f46
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-09-30 14:23:45 +0200

    Finished setup about dots and powerPills
    Now the pacman can eats dot and if it eats all
    of the dots it'll win the game.

--- index.js
+++ index.js
@@ -63,6 +63,43 @@ function gameLoop(pacman, ghosts) {
         gameBoard.moveCharacter(ghost);
     });
     checkCollision(pacman, ghosts);
+
+    // check if Pacman eats a dot
+    if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)) {
+        gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT]);
+        gameBoard.dotCount--;
+        score += 10;
+    }
+
+    // check if Pacman eats a powerpill
+    if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)) { 
+        gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL]);
+
+        pacman.powerPill = true;
+        score += 50;
+
+        clearTimeout(powerPillTimer);
+        powerPillTimer = setTimeout(() => {
+            pacman.powerPill = false;
+        }, POWER_PILL_TIME);
+    }
+
+    // change ghost scare mode depending on PowerPill
+    if (pacman.powerPill !== powerPillActive) {
+        powerPillActive = pacman.powerPill;
+        ghosts.forEach((ghost) => {
+            ghost.isScared = pacman.powerPill;
+        });
+    }
+
+    // check if all dots have been eaten
+    if (gameBoard.dotCount === 0) {
+        gameWin = true;
+        gameOver(pacman, ghosts);
+    }
+
+    // show the score
+    scoreTable.innerHTML = score;
 }
 
 function startGame() {

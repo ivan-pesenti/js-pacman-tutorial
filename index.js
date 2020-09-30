949ea69cf064d6f803e166a47770885ac8f122f5
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-09-30 14:38:43 +0200

    Commit after finish up the tutorial on YouTube
    In the future there will be part II.

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ import   GameBoard  from "./GameBoard";
 import Pacman from "./Pacman";
 import Ghost from "./Ghost";
 
+// Sounds
+import soundDot from "./sounds/munch.wav";
+import soundPill from "./sounds/pill.wav";
+import soundGameStart from "./sounds/game_start.wav";
+import soundGameOver from "./sounds/death.wav";
+import soundGhost from "./sounds/eat_ghost.wav";
+
 // DOM elements
 const gameGrid = document.querySelector('#game');
 const scoreTable = document.querySelector('#score');
@@ -23,7 +30,15 @@ const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
  let powerPillActive = false;
  let powerPillTimer = null;
 
+ // Audio
+ function playAudio(audio) {
+     const soundEffect = new Audio(audio);
+     soundEffect.play();
+ }
+
 function gameOver(pacman, grid) {
+    playAudio(soundGameOver);
+
      document.removeEventListener('keydown', e => {
         pacman.handleKeyInput(e, gameBoard.objectExist);
      });
@@ -40,6 +55,7 @@ function checkCollision(pacman, ghosts) {
 
     if (collidedGhost) {
         if (pacman.powerPill) {
+            playAudio(soundGhost);
             gameBoard.removeObject(collidedGhost.pos, [
                 OBJECT_TYPE.GHOST,
                 OBJECT_TYPE.SCARED,
@@ -66,6 +82,7 @@ function gameLoop(pacman, ghosts) {
 
     // check if Pacman eats a dot
     if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)) {
+        playAudio(soundDot);
         gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT]);
         gameBoard.dotCount--;
         score += 10;
@@ -73,6 +90,8 @@ function gameLoop(pacman, ghosts) {
 
     // check if Pacman eats a powerpill
     if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)) { 
+        playAudio(soundPill);
+
         gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL]);
 
         pacman.powerPill = true;
@@ -103,6 +122,8 @@ function gameLoop(pacman, ghosts) {
 }
 
 function startGame() {
+    playAudio(soundGameStart);
+
     gameWin = false;
     powerPillActive = false;
     score = 0;
